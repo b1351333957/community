c96caf3ea5c30766974b9b7ee9b181d00238fd41
RuiXiang Cheng <1351333957@qq.com>
2020-04-03 08:27:54 +0800

    问答系统

---
+++ src/test/java/com/ruixiang/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruixiang.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
